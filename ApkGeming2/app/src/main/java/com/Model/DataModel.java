f1aa96fbd92825840a3c8a1ee4f24b31325e699c
Hayungdewanto <hayungdewanto9@gmail.com>
2021-07-22 01:01:28 +0700

    up2

--- ApkGeming2/app/src/main/java/com/Model/DataModel.java
+++ ApkGeming2/app/src/main/java/com/Model/DataModel.java
@@ -2,7 +2,7 @@ package com.Model;
 
 public class DataModel {
     private int id_event;
-    private String nama_event, keterangan, gambar;
+    private String nama_event, keterangan, gambar, tgl_mulai_pendaftaran, tgl_akhir_pendaftaran, tanggal_mulai, tanggal_akhir, slot, price ;
 
     public int getId_event() {
         return id_event;
@@ -36,4 +36,51 @@ public class DataModel {
         this.gambar = gambar;
     }
 
+    public String getTgl_mulai_pendaftaran() {
+        return tgl_mulai_pendaftaran;
+    }
+
+    public void setTgl_mulai_pendaftaran(String tgl_mulai_pendaftaran) {
+        this.tgl_mulai_pendaftaran = tgl_mulai_pendaftaran;
+    }
+
+    public String getTgl_akhir_pendaftaran() {
+        return tgl_akhir_pendaftaran;
+    }
+
+    public void setTgl_akhir_pendaftaran(String tgl_akhir_pendaftaran) {
+        this.tgl_akhir_pendaftaran = tgl_akhir_pendaftaran;
+    }
+
+    public String getTanggal_mulai() {
+        return tanggal_mulai;
+    }
+
+    public void setTanggal_mulai(String tanggal_mulai) {
+        this.tanggal_mulai = tanggal_mulai;
+    }
+
+    public String getTanggal_akhir() {
+        return tanggal_akhir;
+    }
+
+    public void setTanggal_akhir(String tanggal_akhir) {
+        this.tanggal_akhir = tanggal_akhir;
+    }
+
+    public String getSlot() {
+        return slot;
+    }
+
+    public void setSlot(String slot) {
+        this.slot = slot;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
--- ApkGeming2/app/src/main/java/com/example/apkgeming/Detail_event.java
+++ ApkGeming2/app/src/main/java/com/example/apkgeming/Detail_event.java
@@ -2,13 +2,59 @@ package com.example.apkgeming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.Model.DataModel;
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 public class Detail_event extends AppCompatActivity {
 
+    public DataModel dm;
+    ImageView gambar;
+    TextView nama,tgl_mp,tgl_ap,tgl_mul,tgl_akh,slot,price,keterangan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_event);
+        gambar = findViewById(R.id.dt_gambar);
+        nama = findViewById(R.id.dt_nama);
+        tgl_mp = findViewById(R.id.dt_tmp);
+        tgl_ap = findViewById(R.id.dt_tap);
+        tgl_mul = findViewById(R.id.dt_tm);
+        tgl_akh = findViewById(R.id.dt_ta);
+        slot = findViewById(R.id.dt_slot);
+        price = findViewById(R.id.dt_price);
+        keterangan = findViewById(R.id.dt_ket);
+        getEvent();
     }
+
+    private void getEvent() {
+        String data = getIntent().getStringExtra("extra");
+        Gson gson = new Gson();
+        dm = gson.fromJson(data, DataModel.class);
+        nama.setText(dm.getNama_event());
+        tgl_mp.setText(dm.getTgl_mulai_pendaftaran());
+        tgl_ap.setText(dm.getTgl_akhir_pendaftaran());
+        tgl_mul.setText(dm.getTanggal_mulai());
+        tgl_akh.setText(dm.getTanggal_akhir());
+        slot.setText(dm.getSlot());
+        price.setText(dm.getPrice());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            keterangan.setText(Html.fromHtml(dm.getKeterangan(), Html.FROM_HTML_MODE_LEGACY));
+        } else
+            keterangan.setText(Html.fromHtml(dm.getKeterangan()));
+        String url = "http://192.168.88.250:8000/images/"+dm.getGambar();
+        Picasso.get()
+                .load(url)
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+                .into(gambar);
+    }
+
+
 }

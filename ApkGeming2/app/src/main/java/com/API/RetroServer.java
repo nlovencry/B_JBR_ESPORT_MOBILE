ffa36d177f1b89a5af4ef8a10769b1f5ca8e23b2
Thufeil-E41191351 <thufeilwartonoputra@gmail.com>
2021-07-25 23:16:32 +0700

    Summary

--- ApkGeming2/app/src/main/java/com/API/RetroServer.java
+++ ApkGeming2/app/src/main/java/com/API/RetroServer.java
@@ -4,11 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-<<<<<<< Updated upstream
-    private static final String baseurl = "http://workshopjti.com/sipasport/public/";
-=======
     private static final String baseurl = "http://192.168.0.14:8000/";
->>>>>>> Stashed changes
     private static Retrofit retro;
 
     public static Retrofit konekRetrofit(){
